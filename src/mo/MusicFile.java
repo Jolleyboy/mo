a08680cf549668e081e15a1d590a50407e809dc1
Joshua Jolley <jol10009@byui.edu>
2014-06-18 14:48:56 -0600

    Added AudioFile and Tag to MusicFile

--- src/mo/MusicFile.java
+++ src/mo/MusicFile.java
@@ -6,18 +6,37 @@
 
 package mo;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.tag.FieldDataInvalidException;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.KeyNotFoundException;
+import org.jaudiotagger.tag.Tag;
+
 /**
  *
  * @author Joshua
  */
 public class MusicFile {
-    private String name;
-    public void setName(String name) {
-        this.name = name;
+    private Tag tag;
+    
+    public MusicFile(AudioFile af) {
+        //get the tag
     }
     
-    public String getName() {
-        return name;
+    public void setArtist(String artist) {
+        try {
+            tag.setField(FieldKey.ARTIST, artist);
+        } catch (KeyNotFoundException ex) {
+            Logger.getLogger(MusicFile.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FieldDataInvalidException ex) {
+            Logger.getLogger(MusicFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public String getArtist() {
+        return tag.getFirst(FieldKey.ARTIST);
     }
     
 }
