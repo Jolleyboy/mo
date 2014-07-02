e4ddc031e3e83fae277771783edb9ebefdd0ef2b
Joshua Jolley <jol10009@byui.edu>
2014-07-02 11:42:23 -0600

    Added test constructor to MusicFile

--- src/mo/Model.java
+++ src/mo/Model.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import mo.MusicFile;
 
@@ -18,7 +19,13 @@ public class Model {
   private ObservableList<MusicFile> listMF;
   private File selectedDirectory;
   private Model() {
-        
+    //for testing purposes only!!!
+    listMF = FXCollections.observableArrayList(
+            new MusicFile("TestTitle","TestArtist","TestAlbum","TestGenre","4:00Test"),
+            new MusicFile("Paradise","Coldplay","Mylo Xyloto","Alternative","4:38"),
+            new MusicFile("Somebody Told Me","The Killers","Hot Fuss","Alternative","3:17"),
+            new MusicFile("It's Time","Imagine Dragons","Continued Silence","Indie Rock","3:59")
+        );
     //to prevent making seperate instances
   }
 
@@ -33,5 +40,13 @@ public class Model {
   
   public void setList(ObservableList<MusicFile> listMF) {
       this.listMF = listMF;
+  }
+  
+  public void setDirectory(File directory) {
+      this.selectedDirectory = directory;
+  }
+  
+  public File getDirectory() {
+      return selectedDirectory;
   }
 }
